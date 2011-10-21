fbbcfdae20f99a44ddd072e25c3adfc5e70b0969
Eric Rochester <err8n@virginia.edu>
2011-10-21 15:23:03 -0400

    Added database configuration and schema creation.

--- src/Network/Ribot/Db.hs
+++ src/Network/Ribot/Db.hs
@@ -13,6 +13,7 @@ module Network.Ribot.Db
     , ConnWrapper
     ) where
 
+import           Control.Monad (forM, forM_, mapM, mapM_)
 import           Database.HDBC
 import           Database.HDBC.Types (IConnection(..), ConnWrapper)
 import           Database.HDBC.Sqlite3 (connectSqlite3)
@@ -23,7 +24,61 @@ import           System.FilePath ((</>))
 -- This connects to the database. It returns a ConnWrapper in order to provide
 -- some flexibility in what database engine I use.
 connectDb :: IO ConnWrapper
-connectDb = findDbFile >>= connectSqlite3 >>= return . ConnWrapper
+connectDb =   findDbFile
+          >>= connectSqlite3
+          >>= initDb
+          >>= createDb
+          >>= return . ConnWrapper
+
+-- This initializes the database by turning on appropriate pragmas.
+initDb :: IConnection c => c -> IO c
+initDb cxn = do
+    runRaw cxn "PRAGMA foreign_keys = ON;"
+    return cxn
+
+-- This creates the database schema and returns the database connection so it
+-- can be used for piping.
+--
+-- (I'm almost certain there's a one-liner for the main definition of this
+-- function, but it's eluding me. I'll have to re-visit this once I have more
+-- Haskell-fu.)
+createDb :: IConnection c => c -> IO c
+createDb =
+    (flip withTransaction) $ \cxn -> do
+        forM_ sql $ (runRaw cxn)
+        return cxn
+    where
+        -- Here are the statements, packaged up so run by create.
+        sql = [ "CREATE TABLE IF NOT EXISTS user ( \
+                        \ id INTEGER PRIMARY KEY, \
+                        \ user TEXT, \
+                        \ last_seen DATETIME \
+                        \ );"
+              , "CREATE TABLE IF NOT EXISTS message ( \
+                        \ id INTEGER PRIMARY KEY, \
+                        \ user_id INTEGER, \
+                        \ posted DATETIME, \
+                        \ FOREIGN KEY (user_id) REFERENCES user(id) \
+                        \ );"
+              , "CREATE TABLE IF NOT EXISTS token ( \
+                        \ id INTEGER PRIMARY KEY, \
+                        \ text TEXT \
+                        \ );"
+              , "CREATE TABLE IF NOT EXISTS position ( \
+                        \ id INTEGER PRIMARY KEY, \
+                        \ token_id INTEGER, \
+                        \ message_id INTEGER, \
+                        \ offset INTEGER, \
+                        \ length INTEGER, \
+                        \ FOREIGN KEY (token_id) REFERENCES token(id), \
+                        \ FOREIGN KEY (message_id) REFERENCES message(id) \
+                        \ );"
+              , "CREATE INDEX IF NOT EXISTS idx_message ON message \
+                        \ (id, user_id, posted);"
+              , "CREATE INDEX IF NOT EXISTS idx_token ON token (id, text);"
+              , "CREATE INDEX IF NOT EXISTS idx_position ON position \
+                        \ (id, token_id, message_id);"
+              ]
 
 -- This finds the database filename. It looks in the user application
 -- directory.
