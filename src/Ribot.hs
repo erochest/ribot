563e4b1c15760aa7940bba217a81e9ee29373ef7
Eric Rochester <err8n@virginia.edu>
2011-10-23 13:39:34 -0400

    Added command-line options.

--- src/Ribot.hs
+++ src/Ribot.hs
@@ -9,17 +9,53 @@ import           Control.Exception (bracket)
 import           Control.Monad.Reader
 import           Network
 import           Network.Ribot.Core
+import           System.Console.CmdArgs
 import           System.IO
 
--- IRC connection settings.
-server = "irc.freenode.org"
-port   = 6667
-chan   = "#err1234567890"       -- #err1234567890 is an ad hoc testing channel.
-nick   = "ribot-bot"
+-- These are the default command-line settings.
+defaultServer :: String
+defaultServer = "irc.freenode.org"
+defaultPort   :: Int
+defaultPort   = 6667
+defaultChan   :: String
+defaultChan   = "#err1234567890"
+defaultNick   :: String
+defaultNick   = "ribot-bot"
+
+-- These are the command line options.
+data Modes
+    = Listen
+        { server  :: String
+        , port    :: Int
+        , channel :: String
+        , nick    :: String
+        }
+    deriving (Show, Data, Typeable)
+
+-- Here are the options for the options.
+ribotModes :: Modes
+ribotModes =
+    Listen
+      { server = defaultServer &= name "s" &= typ "HOST"
+                               &= help ("The IRC server (default is " ++ defaultServer ++ ").")
+      , port = defaultPort &= name "p" &= typ "PORT"
+                           &= help ("The port of the IRC server (default is " ++ (show defaultPort) ++ ").")
+      , channel = defaultChan &= name "c" &= typ "CHANNEL"
+                              &= help ("The channel on the server (default is " ++ defaultChan ++ ").")
+      , nick = defaultNick &= name "n" &= typ "NICKNAME"
+                           &= help ("The IRC nick name (default is " ++ defaultNick ++ ").")
+      } &= details ["This listens on an IRC channel."]
+    &= summary "ribot v0.1"
+    &= program "ribot"
 
 -- The main function.
 main :: IO ()
-main = bracket (connect server port chan nick) disconnect loop
+main = do
+    mode <- cmdArgs ribotModes
+    putStrLn $ show mode
+    case mode of
+        Listen server port chan nick -> 
+            bracket (connect server port chan nick) disconnect loop
     where
         -- Compose functions to get the bot's socket and close it.
         disconnect = hClose . botSocket
