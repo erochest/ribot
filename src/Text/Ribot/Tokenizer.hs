d99cc0fa1d364d06a92bf718205df61a3f02d6d1
Eric Rochester <err8n@virginia.edu>
2011-10-28 16:43:19 -0400

    Everything compiles now.

--- src/Text/Ribot/Tokenizer.hs
+++ src/Text/Ribot/Tokenizer.hs
@@ -2,13 +2,13 @@
 -- This contains the tokenizer for search and generating the MM.
 
 module Text.Ribot.Tokenizer
-    ( Token
-    , TokenList
-    , tokenize
-    , isWord
-    , removeWords
-    , stopList
-    , removeStopList
+    ( -- String
+    -- , [String]
+      tokenize
+    -- , isWord
+    -- , removeWords
+    -- , stopList
+    -- , removeStopList
     ) where
 
 import qualified Data.Char as C
@@ -16,32 +16,43 @@ import           Text.ParserCombinators.Parsec
 
 
 -- First, let's define some type aliases to make things more clear below.
-type Token     = String
-type TokenList = [String]
+-- type Token     = String
+-- type TokenList = [String]
 
--- This is the main function. It breaks a `String` into a `TokenList`. It uses
+-- This is the main function. It breaks a `String` into a `[String]`. It uses
 -- a Parsec parser combinator to do this. The input should be very short, so we
 -- don't need to worry about handling large input or lazy evaluation or
 -- anything.
 --
 -- The two parameters are the source (channel + nick, maybe) and the line to
 -- parse.
-tokenize :: String -> String -> Either ParseError TokenList
+tokenize :: String -> String -> Either ParseError [String]
 tokenize = parse tokenList
 
 -- A `tokenList` is optional whitespace followed by a list of tokens, separated
 -- by and optionally ended by whitespace.
-tokenList = spaces >> token `sepEndBy` (many1 space <|> eof)
+tokenList :: GenParser Char st [String]
+tokenList = spaces >> singleToken `sepEndBy` (skipMany1 space <|> eof)
 
--- A `token` is a punctuation character or a `word`.
-token =   satisfy C.isPunctuation
-      <|> word
+-- A `singleToken` is a punctuation character or a `word`.
+singleToken :: GenParser Char st String
+singleToken =   (satisfy C.isPunctuation >>= \c -> return [c])
+            <|> word
 
 -- A `word` is one or more alpha-numeric characters, optionally followed by a
 -- punctuation mark immediately followed by another word (alphanumber and
 -- another suffix).
-word =
-    many1 alphaNum
-    optional (satisfy C.isPunctuation >> word)
+word :: GenParser Char st String
+word = do
+    base <- many1 alphaNum
+    rest <- optionMaybe suffix
+    return $ case rest of
+        Just rest' -> base ++ rest'
+        Nothing    -> base
 
+suffix :: GenParser Char st String
+suffix = do
+    p <- satisfy C.isPunctuation
+    rest <- word
+    return (p:rest)
 
--- tests/Test/Ribot/Tokenizer.hs
+++ tests/Test/Ribot/Tokenizer.hs
@@ -7,79 +7,84 @@ import           Test.Framework.Providers.HUnit (testCase)
 import           Text.Ribot.Tokenizer
 
 
+onlyRight :: b -> Either a b -> b
+onlyRight def (Left _)    = def
+onlyRight _   (Right val) = val
+
+
 assertWhiteSpace :: Assertion
 assertWhiteSpace =
     assertBool "Whitespace returns no tokens."
                ([] == tokens)
-    where tokens = tokenize "<assertWhiteSpace>" "   \t\n \t"
+    where tokens = onlyRight [] $ tokenize "<assertWhiteSpace>" "   \t\n \t"
 
 assertAlphaNum :: Assertion
 assertAlphaNum =
     assertBool "Alpha-numeric tokens are returned."
                (["alpha", "numeric", "tokens", "are", "returned"] == tokens)
-    where tokens = tokenize "<assertAlphaNum>" "alpha numeric tokens\nare\treturned"
+    where tokens = onlyRight [] $ tokenize "<assertAlphaNum>" "alpha numeric tokens\nare\treturned"
 
 assertNormalized :: Assertion
 assertNormalized =
     assertBool "Alpha-numeric characters are normalized to lower-case."
                (["alpha", "numeric", "characters", "are", "normalized", "to", "lower", "case"] ==
                 tokens)
-    where tokens = tokenize "<assertNormalized>" "AlPhA NuMeRiC ChArAcTeRs aRe nOrMaLiZeD To lOwEr cAsE"
+    where tokens = onlyRight [] $ tokenize "<assertNormalized>" "AlPhA NuMeRiC ChArAcTeRs aRe nOrMaLiZeD To lOwEr cAsE"
 
 assertLeadingWhiteSpace :: Assertion
 assertLeadingWhiteSpace =
     assertBool "Leading whitespace is skipped."
                (["leading", "whitespace", "is", "skipped"] == tokens)
-    where tokens = tokenize "<assertLeadingWhiteSpace>" "  \t  \n  Leading whitespace is skipped"
+    where tokens = onlyRight [] $ tokenize "<assertLeadingWhiteSpace>" "  \t  \n  Leading whitespace is skipped"
 
 assertTrailingWhiteSpace :: Assertion
 assertTrailingWhiteSpace =
     assertBool "Trailing whitespace is skipped."
                (["trailing", "whitespace", "is", "skipped"] == tokens)
-    where tokens = tokenize "<assertTrailingWhiteSpace>" "Trailing whitespace is skipped  \t  \n  "
+    where tokens = onlyRight [] $ tokenize "<assertTrailingWhiteSpace>" "Trailing whitespace is skipped  \t  \n  "
 
 assertPunctuation :: Assertion
 assertPunctuation =
     assertBool "Punctuation is returned as a token."
                ([".", ",", ":", "\"", "$"] == tokens)
-    where tokens = tokenize "<assertPunctuation>" ". , : \" $"
+    where tokens = onlyRight [] $ tokenize "<assertPunctuation>" ". , : \" $"
 
 assertSinglePunctuation :: Assertion
 assertSinglePunctuation =
     assertBool "Punctuation marks are returned singly."
                ([".", ",", ":", "\"", "$"] == tokens)
-    where tokens = tokenize "<assertSinglePunctuation>" ".,:\"$"
+    where tokens = onlyRight [] $ tokenize "<assertSinglePunctuation>" ".,:\"$"
 
 assertLeadingPunctuation :: Assertion
 assertLeadingPunctuation =
     assertBool "Punctuation marks prefixing words create their own tokens."
                (["\"", "'", "this", "!", "s", "a", "?", "quote"] == tokens)
-    where tokens = tokenize "<assertLeadingPunctuation>" "\"'This !s a ?quote"
+    where tokens = onlyRight [] $ tokenize "<assertLeadingPunctuation>" "\"'This !s a ?quote"
 
 assertTrailingPunctuation :: Assertion
 assertTrailingPunctuation =
     assertBool "Punctuation marks suffixing words create their own tokens."
                (["this", ",", "is", ":", "a", "-", "quote", ".", "'", "\""] == tokens)
-    where tokens = tokenize "<assertTrailingPunctuation>" "This, is: a- quote.'\""
+    where tokens = onlyRight [] $ tokenize "<assertTrailingPunctuation>" "This, is: a- quote.'\""
 
 assertContraction :: Assertion
 assertContraction =
     assertBool "Contractions are included with the main token."
                (["can't", "won't", "didn't", "bobby's"] == tokens)
-    where tokens = tokenize "<assertContraction>" "Can't won't didn't Bobby's"
+    where tokens = onlyRight [] $ tokenize "<assertContraction>" "Can't won't didn't Bobby's"
 
 assertNumbers :: Assertion
 assertNumbers =
     assertBool "Commas and decimals in numbers don't split the numbers up."
                (["1,000.00", "3.1415", "1,234,567"] == tokens)
-    where tokens = tokenize "<assertNumbers>" "1,000.00 3.1415 1,234,567"
+    where tokens = onlyRight [] $ tokenize "<assertNumbers>" "1,000.00 3.1415 1,234,567"
 
 assertUrl :: Assertion
 assertUrl =
     assertBool "URLs are their own tokens."
                (["http://www.google.com", "www.scholarslab.org", "/", "erochest@gmail.com"] ==
                 tokens)
-    where tokens = tokenize "<assertUrl>" "http://www.google.com www.scholarslab.org/ erochest@gmail.com"
+    where tokens = onlyRight [] $ tokenize "<assertUrl>" "http://www.google.com www.scholarslab.org/ erochest@gmail.com"
 
 
 tokenizerTests :: [Test]
