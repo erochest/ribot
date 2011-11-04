92ac2bb2a532e0ea0c882e35878c54edc4c9da6f
Eric Rochester <err8n@virginia.edu>
2011-11-03 21:47:59 -0400

    Added tests for indexing.

--- tests/Test/Ribot/Search.hs
+++ tests/Test/Ribot/Search.hs
@@ -6,7 +6,7 @@ import           Database.HDBC
 import           Database.HDBC.Sqlite3 (connectSqlite3, Connection)
 -- import           Database.HDBC.Types (IConnection(..))
 import           Network.Ribot.Db (createDb, initDb)
--- import           Network.Ribot.Search
+import           Network.Ribot.Search
 import           Test.HUnit (Assertion, assertBool)
 import           Test.Framework (Test, testGroup)
 import           Test.Framework.Providers.HUnit (testCase)
@@ -17,10 +17,68 @@ withTempDb = bracket (connectSqlite3 ":memory:" >>= initDb >>= createDb)
 
 assertIndexMessage :: Assertion
 assertIndexMessage =
-    withTempDb $ \_ -> assertBool "assertIndexMessage" False
+    withTempDb $ \cxn -> do
+        insertUser cxn userId "zaphod"
+        insertMsg cxn userId msgId msg
+        tokens <- index cxn msgId msg
+        assertBool "assertIndexMessage indexed tokens"
+                   (tokens == ["all", "aboard", "heart", "gold"])
+        dbTokens <- getDbTokens cxn
+        assertBool "assertIndexMessage tokens in db"
+                   (dbTokens == ["aboard", "all", "gold", "heart"])
+        msgTokens <- getMsgTokens cxn msgId
+        assertBool "assertIndexMessage tokens in msg"
+                   (msgTokens == ["aboard", "all", "gold", "heart"])
+
+    where
+        userId = 3
+        msgId = 4
+        msg = "All aboard the Heart of Gold!"
+
+        insertUser :: IConnection c => c -> Int -> String -> IO ()
+        insertUser cxn uId nick =
+            run cxn
+                "INSERT INTO user (id, username, logging_on) \
+                \ VALUES (?, ?, 0);"
+                [toSql uId, toSql nick] >>
+            return ()
+
+        insertMsg :: IConnection c => c -> Int -> Int -> String -> IO ()
+        insertMsg cxn uId mId m =
+            run cxn
+                "INSERT INTO message (id, user_id, text, posted) \
+                \ VALUES (?, ?, ?, DATETIME('NOW'));"
+                [toSql mId, toSql uId, toSql m] >>
+            return ()
+
+        getDbTokens :: IConnection c => c -> IO [String]
+        getDbTokens cxn = do
+            results <- quickQuery' cxn
+                                   "SELECT text FROM token ORDER BY text;"
+                                   []
+            return [fromSql text | [text] <- results]
+
+        getMsgTokens :: IConnection c => c -> Int -> IO [String]
+        getMsgTokens cxn mId = do
+            results <- quickQuery' cxn
+                                   "SELECT t.text \
+                                   \ FROM token t \
+                                   \ JOIN position p ON p.token_id=t.id \
+                                   \ WHERE p.message_id=?;"
+                                   [toSql mId]
+            return [fromSql text | [text] <- results]
 
 assertTokenizeMessage :: Assertion
-assertTokenizeMessage = assertBool "assertTokenizeMessage" False
+assertTokenizeMessage = do
+    assertBool "assertTokenizeMessage plain"
+               (tokenize "big important words" == expected)
+    assertBool "assertTokenizeMessage stop words"
+               (tokenize "this has big important words" == expected)
+    assertBool "assertTokenizeMessage punctuation"
+               (tokenize "this has big, important words!" == expected)
+    assertBool "assertTokenizeMessage normalize"
+               (tokenize "This has BIG, important words!" == expected)
+    where expected = ["big", "important", "words"]
 
 searchTests :: [Test]
 searchTests =
