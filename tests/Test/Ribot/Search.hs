8eb24e0e8def2b6022343f9664985027ecee02f0
Eric Rochester <err8n@virginia.edu>
2011-11-04 12:25:16 -0400

    Added tests for indexing multiple messages.

--- tests/Test/Ribot/Search.hs
+++ tests/Test/Ribot/Search.hs
@@ -61,7 +61,47 @@ assertIndexMessage =
 assertIndexMessages :: Assertion
 assertIndexMessages =
     withTempDb $ \cxn -> do
-        assertBool "assertIndexMessages" False
+        aId <- insertUser cxn "a"
+        bId <- insertUser cxn "b"
+        ma1Id <- insertMsg cxn aId ma1
+        mb1Id <- insertMsg cxn bId mb1
+
+        let expected = [ ("another",  "b", mb1Id)
+                       , ("indexing", "a", ma1Id)
+                       , ("indexing", "b", mb1Id)
+                       , ("message",  "a", ma1Id)
+                       , ("message",  "b", mb1Id)
+                       , ("messages", "b", mb1Id)
+                       , ("multiple", "a", ma1Id)
+                       , ("multiple", "b", mb1Id)
+                       , ("small",    "a", ma1Id)
+                       , ("test",     "a", ma1Id)
+                       , ("testing",  "b", mb1Id)
+                       ]
+
+        index cxn "a" ma1Id ma1
+        index cxn "b" mb1Id mb1
+
+        tokens <- getIndex cxn
+
+        assertBool "assertIndexMessages" (expected == tokens)
+
+    where
+        ma1 = "This is a small message to test multiple indexing message."
+        mb1 = "This is another message for testing indexing multiple messages."
+
+        getIndex :: IConnection c => c -> IO [(String, String, Int)]
+        getIndex cxn = do
+            results <- quickQuery' cxn
+                                   " SELECT t.text, u.username, m.id \
+                                   \ FROM position p \
+                                   \ JOIN message m ON p.message_id=m.id \
+                                   \ JOIN user u ON m.user_id=u.id \
+                                   \ JOIN token t ON p.token_id=t.id \
+                                   \ ORDER BY t.text, u.username, m.id;"
+                                   []
+            return [(fromSql text, fromSql user, fromSql mid) |
+                    [text, user, mid] <- results]
 
 searchTests :: [Test]
 searchTests =
