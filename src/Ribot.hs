540c3cbed975b3e0af508e2ccb7da0ef50affede
Eric Rochester <err8n@virginia.edu>
2011-11-04 14:01:54 -0400

    Added reindex command line mode.

--- src/Ribot.hs
+++ src/Ribot.hs
@@ -7,10 +7,14 @@ module Main where
 
 import           Control.Exception (bracket)
 import           Control.Monad.Reader
+import qualified Database.HDBC as Db
 import           Network
 import           Network.Ribot.Core
+import           Network.Ribot.Db (connectDb)
+import           Network.Ribot.Search (reindex)
 import           System.Console.CmdArgs
 import           System.IO
+import           Text.Printf
 
 -- These are the default command-line settings.
 defaultServer :: String
@@ -33,25 +37,32 @@ data Modes
         , nick    :: String
         , dbFile  :: Maybe String
         }
+    | Reindex
+       { dbFile   :: Maybe String
+       }
     deriving (Show, Data, Typeable)
 
 -- Here are the options for the options.
 ribotModes :: Modes
-ribotModes =
-    Listen
-      { server = defaultServer &= name "s" &= typ "HOST"
-                               &= help ("The IRC server (default is " ++ defaultServer ++ ").")
-      , port = defaultPort &= name "p" &= typ "PORT"
-                           &= help ("The port of the IRC server (default is " ++ (show defaultPort) ++ ").")
-      , channel = defaultChan &= name "c" &= typ "CHANNEL"
-                              &= help ("The channel on the server (default is " ++ defaultChan ++ ").")
-      , nick = defaultNick &= name "n" &= typ "NICKNAME"
-                           &= help ("The IRC nick name (default is " ++ defaultNick ++ ").")
-      , dbFile = defaultDbFile &= name "d" &= typ "DATABASE-FILE"
-                               &= help "The database file to use for logging."
-      } &= details ["This listens on an IRC channel."]
-    &= summary ("ribot v" ++ ribotVersion)
-    &= program "ribot"
+ribotModes = modes
+    [ Listen
+        { server = defaultServer &= name "s" &= typ "HOST"
+                                 &= help ("The IRC server (default is " ++ defaultServer ++ ").")
+        , port = defaultPort &= name "p" &= typ "PORT"
+                             &= help ("The port of the IRC server (default is " ++ (show defaultPort) ++ ").")
+        , channel = defaultChan &= name "c" &= typ "CHANNEL"
+                                &= help ("The channel on the server (default is " ++ defaultChan ++ ").")
+        , nick = defaultNick &= name "n" &= typ "NICKNAME"
+                             &= help ("The IRC nick name (default is " ++ defaultNick ++ ").")
+        , dbFile = defaultDbFile &= name "d" &= typ "DATABASE-FILE"
+                                 &= help "The database file to use for logging."
+        } &= details ["This listens on an IRC channel."] &= auto
+    , Reindex
+        { dbFile = defaultDbFile &= name "d" &= typ "DATABASE-FILE"
+                                 &= help "The database file to use for logging."
+        } &= details ["This reindexes the messages for searching."]
+    ] &= summary ("ribot v" ++ ribotVersion)
+      &= program "ribot"
 
 -- The main function.
 main :: IO ()
@@ -60,6 +71,12 @@ main = do
     case mode of
         Listen server port chan nick dbFile -> 
             bracket (connect server port chan nick dbFile) disconnect loop
+        Reindex dbFile -> do
+            putStrLn "reindexing database."
+            (msgCount, tknCount) <- bracket (connectDb dbFile)
+                                            Db.disconnect
+                                            reindex
+            printf "indexed %d messages; %d tokens.\n" msgCount tknCount
     where
         -- Compose functions to get the bot's socket and close it.
         disconnect = hClose . botSocket
