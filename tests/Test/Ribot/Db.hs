9d80b228f4cf43b328a39c7493a30542829cb962
Eric Rochester <err8n@virginia.edu>
2011-11-04 12:02:39 -0400

    Refactored DB testing functions into their own module.

---
+++ tests/Test/Ribot/Db.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+-- This contains functions and utilities for working with the database in
+-- tests.
+
+module Test.Ribot.Db
+    ( withTempDb
+    , getScalar
+    , lastInsertRowId
+    , insertUser
+    , insertMsg
+    ) where
+
+import           Control.Exception (bracket)
+import           Data.Convertible (Convertible)
+import qualified Data.List as L
+import           Database.HDBC
+import           Database.HDBC.Sqlite3 (connectSqlite3, Connection)
+import           Network.Ribot.Db (createDb, initDb)
+import           Test.HUnit (Assertion)
+
+-- This creates a temporary database with the schema for ribot and runs the
+-- test `Assertion` in it.
+withTempDb :: (Connection -> Assertion) -> Assertion
+withTempDb = bracket (connectSqlite3 ":memory:" >>= initDb >>= createDb)
+                     disconnect
+
+-- This picks the scalar result from the returned query list.
+getScalar :: Convertible SqlValue a => [[SqlValue]] -> a
+getScalar = fromSql . L.head . L.head
+
+-- This gets the `LAST_INSERT_ROWID()`.
+lastInsertRowId :: IConnection c => c -> IO Int
+lastInsertRowId cxn =
+    quickQuery' cxn "SELECT LAST_INSERT_ROWID();" [] >>=
+    return . getScalar
+
+-- Insert a user into the database. Its returns the user's ID.
+insertUser :: IConnection c => c -> String -> IO Int
+insertUser cxn nick =
+    run cxn
+        "INSERT INTO user (username, logging_on) \
+        \ VALUES (?, 1);"
+        [toSql nick] >>
+    lastInsertRowId cxn
+
+-- Insert a message into the database. It returns the message's ID.
+insertMsg :: IConnection c => c -> Int -> String -> IO Int
+insertMsg cxn uId m =
+    run cxn
+        "INSERT INTO message (user_id, text, posted) \
+        \ VALUES (?, ?, DATETIME('NOW'));"
+        [toSql uId, toSql m] >>
+    lastInsertRowId cxn
+
--- tests/Test/Ribot/Search.hs
+++ tests/Test/Ribot/Search.hs
@@ -1,18 +1,12 @@
-{-# LANGUAGE FlexibleContexts #-}
 
 module Test.Ribot.Search (searchTests) where
 
-import           Control.Exception (bracket)
-import           Data.Convertible (Convertible)
-import qualified Data.List as L
 import           Database.HDBC
-import           Database.HDBC.Sqlite3 (connectSqlite3, Connection)
--- import           Database.HDBC.Types (IConnection(..))
-import           Network.Ribot.Db (createDb, initDb)
 import           Network.Ribot.Search
 import           Test.HUnit (Assertion, assertBool)
 import           Test.Framework (Test, testGroup)
 import           Test.Framework.Providers.HUnit (testCase)
+import           Test.Ribot.Db
 
 assertTokenizeMessage :: Assertion
 assertTokenizeMessage = do
@@ -25,40 +19,6 @@ assertTokenizeMessage = do
     assertBool "assertTokenizeMessage normalize"
                ((tokenize "" "This has BIG, important words!") == expected)
     where expected = ["big", "important", "words"]
-
--- This creates a temporary database with the schema for ribot and runs the
--- test `Assertion` in it.
-withTempDb :: (Connection -> Assertion) -> Assertion
-withTempDb = bracket (connectSqlite3 ":memory:" >>= initDb >>= createDb)
-                     disconnect
-
--- This picks the scalar result from the returned query list.
-getScalar :: Convertible SqlValue a => [[SqlValue]] -> a
-getScalar = fromSql . L.head . L.head
-
--- This gets the `LAST_INSERT_ROWID()`.
-lastInsertRowId :: IConnection c => c -> IO Int
-lastInsertRowId cxn =
-    quickQuery' cxn "SELECT LAST_INSERT_ROWID();" [] >>=
-    return . getScalar
-
--- Insert a user into the database. Its returns the user's ID.
-insertUser :: IConnection c => c -> String -> IO Int
-insertUser cxn nick =
-    run cxn
-        "INSERT INTO user (username, logging_on) \
-        \ VALUES (?, 1);"
-        [toSql nick] >>
-    lastInsertRowId cxn
-
--- Insert a message into the database. It returns the message's ID.
-insertMsg :: IConnection c => c -> Int -> String -> IO Int
-insertMsg cxn uId m =
-    run cxn
-        "INSERT INTO message (user_id, text, posted) \
-        \ VALUES (?, ?, DATETIME('NOW'));"
-        [toSql uId, toSql m] >>
-    lastInsertRowId cxn
 
 -- This tests indexing a single message.
 assertIndexMessage :: Assertion
