c5410f523c336d5e87851585312291ee0a843f33
Eric Rochester <err8n@virginia.edu>
2011-11-15 09:14:18 -0500

    Added failing tests and stubs for generator functions.

--- src/Text/Ribot/Generate.hs
+++ src/Text/Ribot/Generate.hs
@@ -4,6 +4,45 @@
 -- items, and by extension, a string of many items.
 
 module Text.Ribot.Generate
-    (
+    ( TextGenerator
+    , mkTextGenerator
+    , mostLikely
+    , randomContinuation
+    , chain
+    , triples
     ) where
+
+-- This is the data type for holding the information about the statistical
+-- model. Currently, this is just a stub.
+data TextGenerator a = TextGenerator ()
+
+-- This is the constructor for `TextGenerator` data.
+mkTextGenerator :: [(a, a, a)] -> TextGenerator a
+mkTextGenerator _ = TextGenerator ()
+
+-- This returns the most likely continuation of the sequence given. If there's
+-- no data for that sequence, `Nothing` is returned.
+mostLikely :: TextGenerator a -> (a, a) -> Maybe a
+mostLikely _ _ = Nothing
+
+-- This returns a continuation for the sequence. It does using a random
+-- selecting weighted by the frequency of each continuation. If there aren't
+-- any continuations, `Nothing` is returned.
+randomContinuation :: TextGenerator a -> (a, a) -> Maybe a
+randomContinuation _ _ = Nothing
+
+-- This creates an infinite chain of items. You can limit it using `take` or
+-- `takeWhile` or something. The item given is the item to use for placeholders
+-- at the beginning and end of sequences.
+chain :: TextGenerator a -> a -> [a]
+chain _ _ = []
+
+-- This breaks a list of items into a list of overlapping triples. The first
+-- position is for a boundary marker, so this call,
+--
+--     triples -1 [1..5]
+--     [(-1, -1, 1), (-1, 1, 2), (1, 2, 3), (2, 3, 4), (3, 4, 5), (4, 5, -1),
+--      (5, -1, -1)]
+triples :: a -> [a] -> [(a, a, a)]
+triples _ _ = []
 
--- tests/Test/Ribot/Generate.hs
+++ tests/Test/Ribot/Generate.hs
@@ -3,25 +3,126 @@ module Test.Ribot.Generate
     ( generateTests
     ) where
 
+import qualified Data.List as L
+import           System.Random
 import           Test.HUnit (Assertion, assertBool)
 import           Test.Framework (Test, testGroup)
 import           Test.Framework.Providers.HUnit (testCase)
 import           Text.Ribot.Generate
 
-assertTraining :: Assertion
-assertTraining = assertBool "assertTraining" False
+-- This is from the sample trigrams for the Google Web 1T 5-gram v1.
+testTrain :: TextGenerator String
+testTrain = mkTextGenerator . L.concat $ trainingData
+    where
+        trainingData = [ L.replicate 55   ("ceramics", "collectables", "collectibles")
+                       , L.replicate 130  ("ceramics", "collectables", "fine")
+                       , L.replicate 52   ("ceramics", "collected", "by")
+                       , L.replicate 50   ("ceramics", "collectible", "pottery")
+                       , L.replicate 45   ("ceramics", "collectibles", "cooking")
+                       , L.replicate 144  ("ceramics", "collection", ",")
+                       , L.replicate 247  ("ceramics", "collection", ".")
+                       , L.replicate 120  ("ceramics", "collection", "</S>")
+                       , L.replicate 43   ("ceramics", "collection", "and")
+                       , L.replicate 52   ("ceramics", "collection", "at")
+                       , L.replicate 68   ("ceramics", "collection", "is")
+                       , L.replicate 76   ("ceramics", "collection", "of")
+                       , L.replicate 59   ("ceramics", "collection", "|")
+                       , L.replicate 66   ("ceramics", "collections", ",")
+                       , L.replicate 60   ("ceramics", "collections", ".")
+                       , L.replicate 46   ("ceramics", "combined", "with")
+                       , L.replicate 69   ("ceramics", "come", "from")
+                       , L.replicate 660  ("ceramics", "comes", "from")
+                       , L.replicate 109  ("ceramics", "community", ",")
+                       , L.replicate 212  ("ceramics", "community", ".")
+                       , L.replicate 61   ("ceramics", "community", "for")
+                       , L.replicate 53   ("ceramics", "companies", ".")
+                       , L.replicate 173  ("ceramics", "companies", "consultants")
+                       , L.replicate 4432 ("ceramics", "company", "!")
+                       , L.replicate 133  ("ceramics", "company", ",")
+                       , L.replicate 92   ("ceramics", "company", ".")
+                       , L.replicate 41   ("ceramics", "company", "</S>")
+                       , L.replicate 145  ("ceramics", "company", "facing")
+                       , L.replicate 181  ("ceramics", "company", "in")
+                       , L.replicate 137  ("ceramics", "company", "started")
+                       , L.replicate 87   ("ceramics", "company", "that")
+                       , L.replicate 76   ("ceramics", "component", "(")
+                       , L.replicate 85   ("ceramics", "composed", "of")
+                       , L.replicate 56   ("ceramics", "composites", "ferrites")
+                       , L.replicate 41   ("ceramics", "composition", "as")
+                       , L.replicate 51   ("ceramics", "computer", "graphics")
+                       , L.replicate 52   ("ceramics", "computer", "imaging")
+                       , L.replicate 92   ("ceramics", "consist", "of")
+                       ]
 
 assertMostLikely :: Assertion
-assertMostLikely = assertBool "assertMostLikely" False
+assertMostLikely = mapM_ (uncurry aml') inputs
+    where
+        hmm = testTrain
+        aml' obs expected = assertBool ("most likely " ++ (show obs) ++ " =>" ++ (show ml))
+                                       (expected == ml)
+                            where ml = mostLikely hmm obs
+        inputs = [ (("ceramics", "collectables"), Just "fine")
+                 , (("ceramics", "collection"), Just ".")
+                 , (("ceramics", "comes"), Just "from")
+                 , (("ceramics", "company"), Just "!")
+                 , (("ceramics", "not here"), Nothing)
+                 ]
 
-assertPropabilityPick :: Assertion
-assertPropabilityPick = assertBool "assertPropabilityPick" False
+-- Random numbers will look something like this:
+--
+--     ghci> setStdGen $ mkStdGen 0
+--     ghci> mapM (\x -> randomRIO (0.0, 1.0) >>= return . (\y -> (x, y))) [1..10]
+--     [(1,0.9872770353686064),(2,9.953808039426804e-4),(3,0.9737700945697725),(4,0.130280438112095),(5,0.2037222092039883),(6,0.5737648080103099),(7,0.9673066593240947),(8,0.6253049722872674),(9,0.5327889025211334),(10,0.45740725565701723)]
+
+resetRandom :: IO ()
+resetRandom = setStdGen $ mkStdGen 0
+
+assertRandomContinuation :: Assertion
+assertRandomContinuation = do
+    resetRandom
+    mapM_ (uncurry app) inputs
+    where
+        hmm = testTrain
+        app obs expected = assertBool ("random continuation " ++ (show obs) ++ " => " ++ (show pp))
+                                 (expected == pp)
+                            where pp = randomContinuation hmm obs
+        inputs = [ (("ceramics", "comes"), Just "from")      -- [(1,0.9872770353686064)
+                 , (("ceramics", "community"), Just ".")     -- ,(2,9.953808039426804e-4)
+                 , (("ceramics", "company"), Just ".")       -- ,(3,0.9737700945697725)
+                 , (("cermaics", "not here"), Nothing)
+                 ]
+-- ,(4,0.130280438112095)
+-- ,(5,0.2037222092039883)
+-- ,(6,0.5737648080103099)
+-- ,(7,0.9673066593240947)
+-- ,(8,0.6253049722872674)
+-- ,(9,0.5327889025211334)
+-- ,(10,0.45740725565701723)]
+
+assertTriples :: Assertion
+assertTriples =
+    assertBool ("assertTriples => " ++ (show obs)) (expected == obs)
+    where
+        expected = [ ('#', '#', '0')
+                   , ('#', '0', '1')
+                   , ('0', '1', '2')
+                   , ('1', '2', '3')
+                   , ('2', '3', '4')
+                   , ('3', '4', '5')
+                   , ('4', '5', '6')
+                   , ('5', '6', '7')
+                   , ('6', '7', '8')
+                   , ('7', '8', '9')
+                   , ('8', '9', '#')
+                   , ('9', '#', '#')
+                   ]
+        obs = triples '#' "0123456789"
 
 generateTests :: [Test]
 generateTests =
-    [ testGroup "generate" [ testCase "train" assertTraining
-                           , testCase "most-likely" assertMostLikely
-                           , testCase "probability-pick" assertPropabilityPick
+    [ testGroup "generate" [ testCase "most-likely" assertMostLikely
+                           , testCase "random-continuation" assertRandomContinuation
+                           , testCase "triples" assertTriples
                            ]
     ]
 
