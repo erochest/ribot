d9150d14fd2d564592f7c88b7c1ac87472c453f8
Eric Rochester <err8n@virginia.edu>
2011-11-18 15:48:16 -0500

    v0.4.1

--- src/Network/Ribot/Irc/Commands.hs
+++ src/Network/Ribot/Irc/Commands.hs
@@ -29,7 +29,7 @@ import           Text.Ribot.Generate (mimic)
 
 -- This is the version, for the command line and the !version command.
 ribotVersion :: String
-ribotVersion =  "0.4.0"
+ribotVersion =  "0.4.1"
 
 -- This is the help message for the bot.
 helpMessage :: [String]
