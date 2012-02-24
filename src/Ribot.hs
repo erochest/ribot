d8da80bb9abb3be5c3754ffae65adb26f1a37cb5
Eric Rochester <err8n@virginia.edu>
2012-02-24 14:25:44 -0500

    Read configuration from a file.

--- src/Ribot.hs
+++ src/Ribot.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 -- This is the entry point for Ribot.
 
@@ -6,7 +7,10 @@ module Main where
 import           Control.Applicative
 import           Control.Concurrent
 import           Control.Concurrent.Chan
+import qualified Data.Configurator as C
+import           Data.Configurator.Types (Config)
 import qualified Data.Set as S
+import qualified Data.Text as T
 import           Data.Version (showVersion)
 import           Network
 import           Network.BSD
@@ -38,35 +42,30 @@ ribotModes = modes
       &= program "ribot"
 
 
--- Some defaults.
-ribotServer :: HostName
-ribotServer = "irc.freenode.org"
-
-ribotPort :: PortID
-ribotPort = PortNumber 6667
-
-ribotChan :: S.Set String
-ribotChan = S.singleton "#err1234567890"
-
-ribotNick :: String
-ribotNick = "ribtest"
-
 main :: IO ()
 main = do
     mode <- cmdArgs ribotModes
     case config mode of
-        Nothing -> putStrLn "You must specify a configuration file."
-        Just cfgFile -> do
-            config <- initConfig
-            parts  <- initParts
-            tids   <- simpleBot config parts
-            (logger config) Important "Press ENTER to quit."
-            getLine
-            mapM_ killThread tids
+        Nothing       -> putStrLn "You must specify a configuration file."
+        Just fileName -> do
+            configFile <- C.load [ C.Optional "/etc/ribot/ribot.cfg"
+                                 , C.Optional "$(HOME)/.ribot.cfg"
+                                 , C.Required fileName
+                                 ]
+            config' <- initConfig configFile
 
-initParts :: (BotMonad m) => IO [m ()]
-initParts = do
-    (_, chanParts) <- initChannelsPart ribotChan
+            case config' of
+                Nothing     -> putStrLn "You must are missing configuration keys."
+                Just config -> do
+                    parts  <- initParts config
+                    tids   <- simpleBot config parts
+                    (logger config) Important "Press ENTER to quit."
+                    getLine
+                    mapM_ killThread tids
+
+initParts :: (BotMonad m) => BotConf -> IO [m ()]
+initParts config = do
+    (_, chanParts) <- initChannelsPart $ channels config
     return [ pingPart
            , nickUserPart
            , chanParts
@@ -74,19 +73,33 @@ initParts = do
            , helloPart
            ]
 
-initConfig :: IO BotConf
-initConfig = do
-    host <- getHostName
-    return (BotConf
-        (Just writeMsg)
-        (stdoutLogger Debug)
-        ribotServer
-        ribotPort
-        ribotNick
-        "!"
-        (User ribotNick host ribotServer ribotNick)
-        ribotChan
-        )
+initConfig :: Config -> IO (Maybe BotConf)
+initConfig config = getHostName >>= initConfig' config
+    where
+        initConfig' :: Config -> HostName -> IO (Maybe BotConf)
+        initConfig' config hostName = do
+            server <- C.lookup config "irc.server"
+            port   <- C.lookupDefault 6667 config "irc.port"
+            nick   <- C.lookup config "irc.nick"
+            chan   <- C.lookup config "irc.channel"
+            return $ maybeConfig hostName server port nick chan
+
+        maybeConfig :: HostName -> Maybe HostName -> Int -> Maybe String
+                    -> Maybe String -> Maybe BotConf
+        maybeConfig host server port nick chan = do
+            server' <- server
+            nick'   <- nick
+            chan'   <- chan
+            return $ BotConf
+                { channelLogger = Just writeMsg
+                , logger        = stdoutLogger Debug
+                , host          = server'
+                , port          = PortNumber $ fromIntegral port
+                , nick          = nick'
+                , commandPrefix = "!"
+                , user          = User nick' host server' nick'
+                , channels      = S.singleton chan'
+                }
 
 writeMsg :: Chan Message -> IO ()
 writeMsg chan = readChan chan >>= putStrLn . ("MESSAGE: " ++) . show
