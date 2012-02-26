25a412640157b577a5aa1344c93b4864a76119ba
Eric Rochester <err8n@virginia.edu>
2012-02-26 15:53:29 -0500

    Refactored Data.Ribot.Config and added documentation.

--- src/Data/Ribot/Config.hs
+++ src/Data/Ribot/Config.hs
@@ -8,6 +8,7 @@ module Data.Ribot.Config
     , createBotConf
     ) where
 
+import           Control.Applicative
 import           Data.Configurator
 import           Data.Configurator.Types (Config)
 import qualified Data.Set as S
@@ -37,19 +38,29 @@ readConfig cliConfigPath = do
 -- This takes a `Config` value and converts it to a `BotConf`. If any required
 -- values are missing from the `Config`, `Nothing` is returned.
 createBotConf :: Config -> (Chan Message -> IO ()) -> IO (Maybe BotConf)
-createBotConf config channelLogger = getHostName >>= initConfig' config
+createBotConf config channelLogger = getHostName >>= createConf config
     where
-        initConfig' :: Config -> HostName -> IO (Maybe BotConf)
-        initConfig' config hostName = do
-            server <- lookup config "irc.server"
-            port   <- lookupDefault 6667 config "irc.port"
-            nick   <- lookup config "irc.nick"
-            chan   <- lookup config "irc.channel"
-            logger <- getLogger config
-            return $ maybeConfig hostName server port nick chan logger
 
-        maybeConfig :: HostName -> Maybe HostName -> Int -> Maybe String
-                    -> Maybe String -> Logger -> Maybe BotConf
+        -- This removes the IO context from the parameters, maybe creates a
+        -- `BotConf` and re-inserts it into the IO context.
+        createConf :: Config -> HostName -> IO (Maybe BotConf)
+        createConf config hostName =
+            maybeConfig hostName <$> lookup config "irc.server"
+                                 <*> lookupDefault 6667 config "irc.port"
+                                 <*> lookup config "irc.nick"
+                                 <*> lookup config "irc.channel"
+                                 <*> getLogger config
+
+        -- This takes the parameters, some maybe not provided, and returns a
+        -- `BotConf`. If any of the parameters wrapped in `Maybe` aren't given
+        -- (i.e., are `Nothing`), then return `Nothing`.
+        maybeConfig :: HostName             -- Client host
+                    -> Maybe HostName       -- IRC server
+                    -> Int                  -- IRC port
+                    -> Maybe String         -- Nickname
+                    -> Maybe String         -- Channel
+                    -> Logger               -- logger
+                    -> Maybe BotConf        -- Configuration
         maybeConfig host server port nick chan logger = do
             server' <- server
             nick'   <- nick
@@ -65,20 +76,28 @@ createBotConf config channelLogger = getHostName >>= initConfig' config
                 , channels      = S.singleton chan'
                 }
 
+        -- This parses out the configuration options for the logger and returns
+        -- it.
         getLogger :: Config -> IO Logger
         getLogger config =
             getLogger' <$> lookupDefault 1 config "log.level"
                        <*> lookupDefault "STDOUT" config "log.file"
 
+        -- This takes the configuration options and turns them into a concrete
+        -- `Logger`.
         getLogger' :: Int -> FilePath -> Logger
         getLogger' 0 _            = nullLogger
         getLogger' level "STDOUT" = stdoutLogger (getLevel level)
         getLogger' level file     = appendLogger (getLevel level) file
 
+        -- This `Logger` appends the input line to the file, if its level
+        -- matches or is greater than the level this was created with.
         appendLogger :: LogLevel -> FilePath -> Logger
         appendLogger level file msgLevel msg =
             when (msgLevel >= level) (appendFile file $ msg ++ "\n")
 
+        -- This converts a `log.level` value from the configuration file into a
+        -- `LogLevel`.
         getLevel :: Int -> LogLevel
         getLevel 1 = Important
         getLevel 2 = Normal
